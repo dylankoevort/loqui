bb18635ad70f46bf2799309b38b9a900128adb2c
Dylan Koevort <koevortd@gmail.com>
2023-07-31 21:28:50 +0200

    Loqui asset changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,8 @@
-import LlamaIcon from './llamaIcon.svg';
+import IconClear from './iconClear.svg';
+import IconClearCropped from './IconClearCropped.svg';
+import IconWhite from './iconWhite.svg';
+import IconWhiteCropped from './iconWhiteCropped.svg';
+import IconJpg from './icon.jpg';
 import QrCode from './qr-code.svg';
 
-export { LlamaIcon, QrCode  };
+export { IconClear,IconClearCropped, IconWhite, IconWhiteCropped, IconJpg, QrCode  };
--- src/components/Landing/landingContainer/LandingContainer.jsx
+++ src/components/Landing/landingContainer/LandingContainer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyledLandingContainer, StyledLandingHeader, StyledLandingMain } from './styledComponents';
 import Login from '../login';
-import { LlamaIcon } from 'src/assets';
+import { IconWhite } from 'src/assets';
 
 const LandingContainer = (props) => {
 	return (
 		<StyledLandingContainer>
 			<StyledLandingHeader>
 				<div className="icon">
-					<img src={LlamaIcon} alt="Loqui" />
+					<img src={IconWhite} alt="Loqui" />
 				</div>
 				<div className="title">Loqui</div>
 			</StyledLandingHeader>
