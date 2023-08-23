edaf208f677a51d22f260745327dca557217b920
Dylan Koevort <koevortd@gmail.com>
2023-08-23 16:19:55 +0200

    Cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		react({
-			jsxRuntime: 'classic'
-		})
-	],
-	build: {
-		sourcemap: 'hidden',
-		minify: true
-	},
-	resolve: {
-		alias: {
-			src: '/src',
-			components: '/src/components',
-			assets: '/src/assets',
-			helpers: '/src/helpers',
-			store: '/src/store'
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [
+			react({
+				jsxRuntime: 'classic'
+			})
+		],
+		build: {
+			sourcemap: 'hidden',
+			minify: true
+		},
+		resolve: {
+			alias: {
+				src: '/src',
+				components: '/src/components',
+				assets: '/src/assets',
+				helpers: '/src/helpers',
+				store: '/src/store'
+			}
+		},
+		define: {
+			'process.env': env
 		}
-	}
+	};
 });
